7277e608b98ae0fa58fbaa428d971929d1b1f51d
Emily Crandall Fleischman <emilycf@mit.edu>
2017-08-09 23:02:19 -0400

    Fix typoes

--- sounds/cache.go
+++ sounds/cache.go
@@ -21,6 +21,8 @@ func NewCache() *Cache {
 	}
 }
 
+// LoadFile loads a file and caches its contents, or returns the cached
+// contents if they are as new as the file
 func (c *Cache) LoadFile(filename string) (rl *RuleList, err error) {
 	info, err := os.Stat(filename)
 	if err != nil {
@@ -47,6 +49,7 @@ func (c *Cache) LoadFile(filename string) (rl *RuleList, err error) {
 	return rl, nil
 }
 
+// ApplyFile applies a sound change file to a word
 func (c *Cache) ApplyFile(word, filename string) (output string, debug []string, err error) {
 	rl, err := c.LoadFile(filename)
 	if err != nil {
@@ -55,6 +58,7 @@ func (c *Cache) ApplyFile(word, filename string) (output string, debug []string,
 	return rl.Apply(word)
 }
 
+// ApplyFiles applies a series of files to a word
 func (c *Cache) ApplyFiles(word string, files ...string) (output string, debug []string, err error) {
 	debugs := make([][]string, len(files))
 	output = word
@@ -65,4 +69,16 @@ func (c *Cache) ApplyFiles(word string, files ...string) (output string, debug [
 		}
 	}
 	return output, stringSliceConcat(debugs...), nil
+}
+
+// ApplyPairs applies a series of sound changes to a word, using a prefix for
+// all filenames
+func (c *Cache) ApplyPairs(word, prefix string, names ...string) (string, []string, error) {
+	pairs, err := Pairs(names...)
+	var debug []string
+	if err != nil {
+		return "", debug, err
+	}
+	files := prefixSlice(pairs, prefix)
+	return c.ApplyFiles(word, files...)
 }
