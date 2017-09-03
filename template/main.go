73fe5f2a37196be0296e081ef4207c9406b6cb9b
Emily Crandall Fleischman <emilycf@mit.edu>
2017-09-03 14:44:46 -0400

    Add template execution and sc files to template

--- template/main.go
+++ template/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"github.com/zyxw59/conlang/sounds"
 	"log"
@@ -21,13 +22,24 @@ func fatal(err error) {
 func main() {
 	var err error
 
-	filename := flag.String("i", "", "template file")
+	prefix := flag.String("p", "", "prefix for sound change files")
 	flag.Parse()
+	filename := flag.Arg(0)
 
-	rl := sounds.NewRuleList()
+	var rl *sounds.RuleList
+	cache := sounds.NewCache()
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-	t := template.New(filepath.Base(*filename))
+	t := template.New(filepath.Base(filename))
 	t = t.Funcs(template.FuncMap{
+		"ApplyPairs": func(word string, names ...string) (string, error) {
+			output, _, err := cache.ApplyPairs(word, *prefix, names...)
+			return output, err
+		},
+		"Execute": func(templ, word string) (string, error) {
+			wr := new(bytes.Buffer)
+			err := t.ExecuteTemplate(wr, templ, word)
+			return wr.String(), err
+		},
 		"Match": func(word, rule string) ([]sounds.Match, error) {
 			parsed, err := sounds.ParseRule(rule)
 			if err != nil {
@@ -50,14 +62,14 @@ func main() {
 			return output, err
 		},
 	})
-	t, err = t.ParseFiles(*filename)
+	t, err = t.ParseFiles(filename)
 	fatal(err)
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		word := input.Text()
+		rl = sounds.NewRuleList()
 		err = t.Execute(w, word)
 		fatal(err)
 		w.Flush()
-		rl = sounds.NewRuleList()
 	}
 }
